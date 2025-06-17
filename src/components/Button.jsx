f21fcbb516ac1902826478caad5fb4512c5025e0
Malvin A <malvinaudriansyah77@gmail.com>
2025-06-17 09:47:05 +0700

    changed favicon into Tenplus logo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ children }) => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="cursor-pointer px-6 py-2 bg-[#E1E43D] rounded-full w-auto font-medium text-lg text-black">
+        <button className="cursor-pointer px-6 py-2 bg-[#E1E43D] rounded-full w-auto font-medium text-lg text-black hover:bg-[#f6f885] transition duration-300 ease-in-out">
           <p>{children}</p>
         </button>
       </a>
